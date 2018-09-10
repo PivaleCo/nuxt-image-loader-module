56a9de804e796f96f03e06aab6bf67bcf9de22eb
Barry Fisher <barry.fisher@reallifedigital.com>
2018-09-10 23:58:02 +0100

    Set typescript compiler target to es2018 to prevent transpilation.

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const path = require("path");
 const fs = require("fs");
@@ -275,15 +267,13 @@ module.exports = function imageLoader(moduleOptions) {
     if (buildType === 'generate') {
         const generateDir = this.nuxt.options.generate.dir;
         process.$imageLoaderRegistry = [];
-        this.nuxt.hook('generate:done', function (generator) {
-            return __awaiter(this, void 0, void 0, function* () {
-                yield addForceGeneratedImages(moduleOptions);
-                generateStaticImages({
-                    imagePaths: process.$imageLoaderRegistry,
-                    imageStyles: moduleOptions.imageStyles,
-                    imagesBaseDir: moduleOptions.imagesBaseDir,
-                    generateDir
-                });
+        this.nuxt.hook('generate:done', async function (generator) {
+            await addForceGeneratedImages(moduleOptions);
+            generateStaticImages({
+                imagePaths: process.$imageLoaderRegistry,
+                imageStyles: moduleOptions.imageStyles,
+                imagesBaseDir: moduleOptions.imagesBaseDir,
+                generateDir
             });
         });
     }
